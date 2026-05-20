c49471bb80acbc79c46f9eab49b268b0fe4d5bfb
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-20 23:40:52 +0300

    feat(auth): add jwt strategy and guard

---
+++ apps/backend/src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { AuthService } from '../auth.service';
+import type { CurrentUserPayload } from '../types/current-user-payload.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('jwt.accessSecret'),
+    });
+  }
+
+  validate(payload: CurrentUserPayload) {
+    return this.authService.validateJwtPayload(payload);
+  }
+}
