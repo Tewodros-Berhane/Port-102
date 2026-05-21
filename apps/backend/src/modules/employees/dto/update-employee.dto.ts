152c94c589018015cef3811b18b3755fac7dee86
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-22 02:17:32 +0300

    feat(employees): define employee update dto

--- apps/backend/src/modules/employees/dto/update-employee.dto.ts
+++ apps/backend/src/modules/employees/dto/update-employee.dto.ts
@@ -1,2 +1,48 @@
-export class UpdateEmployeeDto {}
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 
+export class UpdateEmployeeDto {
+  @IsString()
+  @IsOptional()
+  employeeNumber?: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  lastName?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string | null;
+
+  @IsString()
+  @IsOptional()
+  phone?: string | null;
+
+  @IsString()
+  @IsOptional()
+  jobTitle?: string | null;
+
+  @Type(() => Number)
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  departmentId?: number | null;
+
+  @IsDateString()
+  @IsOptional()
+  hireDate?: string | null;
+}
