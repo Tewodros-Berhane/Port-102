3e9ddb0b7b27870273b391df39590958fd9f1cfb
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-26 01:21:05 +0300

    feat(rooms): add status log query dto

---
+++ apps/backend/src/modules/rooms/dto/get-room-status-logs-query.dto.ts
@@ -0,0 +1,29 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+export class GetRoomStatusLogsQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    minimum: 1,
+    default: 1,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  page = 1;
+
+  @ApiPropertyOptional({
+    example: 20,
+    minimum: 1,
+    maximum: 100,
+    default: 20,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  @IsOptional()
+  limit = 20;
+}
