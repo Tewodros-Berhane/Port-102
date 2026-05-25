e864286f1ca9a523b3059c72d0d3db3a33233f84
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-26 01:21:00 +0300

    feat(room-types): add list query dto part 1

---
+++ apps/backend/src/modules/room-types/dto/get-room-types-query.dto.ts
@@ -0,0 +1,32 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class GetRoomTypesQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    minimum: 1,
+    default: 1,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  page = 1;
+
+  @ApiPropertyOptional({
+    example: 20,
+    minimum: 1,
+    maximum: 100,
+    default: 20,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
