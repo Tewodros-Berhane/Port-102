b1da85181eb11a6e0138bebbee890f24ee529a6f
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-30 00:08:21 +0300

    feat(front-desk): add query dto segment 1

---
+++ apps/backend/src/modules/front-desk/dto/front-desk-query.dto.ts
@@ -0,0 +1,20 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+class FrontDeskPaginatedQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    minimum: 1,
+    default: 1,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
