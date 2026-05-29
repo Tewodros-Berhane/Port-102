31503434f9e7bdd412f92f13ad4e5ae5b5470afb
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-30 00:08:23 +0300

    feat(front-desk): add module segment 2

--- apps/backend/src/modules/front-desk/front-desk.module.ts
+++ apps/backend/src/modules/front-desk/front-desk.module.ts
@@ -4,3 +4,10 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { FrontDeskController } from './front-desk.controller';
 import { FrontDeskRepository } from './repositories/front-desk.repository';
 import { FrontDeskService } from './front-desk.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [FrontDeskController],
+  providers: [FrontDeskService, FrontDeskRepository, PermissionsGuard],
+})
+export class FrontDeskModule {}
