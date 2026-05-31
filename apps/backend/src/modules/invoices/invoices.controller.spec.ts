eb86deccd64924737d60e9abac47c5630d8781cc
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-31 12:37:04 +0300

    billing: add invoices controller spec tests

---
+++ apps/backend/src/modules/invoices/invoices.controller.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PermissionsGuard } from '../../common/guards/permissions.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { InvoicesController } from './invoices.controller';
+import { InvoicesService } from './invoices.service';
+
+describe('InvoicesController', () => {
+  let controller: InvoicesController;
+  const invoicesService = {};
+  const guard = {
+    canActivate: jest.fn(() => true),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [InvoicesController],
+      providers: [
+        {
+          provide: InvoicesService,
+          useValue: invoicesService,
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue(guard)
+      .overrideGuard(PermissionsGuard)
+      .useValue(guard)
+      .compile();
+
+    controller = module.get<InvoicesController>(InvoicesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
