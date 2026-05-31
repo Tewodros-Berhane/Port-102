d2267c298ae8165c58a3509ec2723acdf4f2975f
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-05-31 12:37:05 +0300

    billing: add invoices service spec tests

---
+++ apps/backend/src/modules/invoices/invoices.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { InvoicesService } from './invoices.service';
+import { InvoicesRepository } from './repositories/invoices.repository';
+import { ReceiptsRepository } from './repositories/receipts.repository';
+
+describe('InvoicesService', () => {
+  let service: InvoicesService;
+  const invoicesRepository = {};
+  const receiptsRepository = {};
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        InvoicesService,
+        {
+          provide: InvoicesRepository,
+          useValue: invoicesRepository,
+        },
+        {
+          provide: ReceiptsRepository,
+          useValue: receiptsRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<InvoicesService>(InvoicesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
