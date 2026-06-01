12b7625fdd7f43562d541900e1aea1d2bd35216c
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-06-02 00:53:52 +0300

    authz: add any-permission decorator
    Commit 3 of 57 for housekeeping operations backend implementation.

--- apps/backend/src/common/decorators/permissions.decorator.ts
+++ apps/backend/src/common/decorators/permissions.decorator.ts
@@ -1,6 +1,10 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const REQUIRED_PERMISSIONS_KEY = 'requiredPermissions';
+export const ANY_REQUIRED_PERMISSIONS_KEY = 'anyRequiredPermissions';
 
 export const Permissions = (...permissions: string[]) =>
   SetMetadata(REQUIRED_PERMISSIONS_KEY, permissions);
+
+export const AnyPermissions = (...permissions: string[]) =>
+  SetMetadata(ANY_REQUIRED_PERMISSIONS_KEY, permissions);
